99ba5794c04064d1f51402cff449e48d66022849
DimitarDTsonev <mitkoconev63@gmail.com>
2025-01-27 17:10:10 +0200

    remove and change files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,24 @@
 provider "aws" {
-  region = "eu-north-1"  # Replace with your desired AWS region
+  region = "eu-north-1"
 }
 
 resource "aws_security_group" "web_sg" {
   name        = "web-security-group-${random_string.suffix.result}"
   description = "Allow HTTP inbound"
-  vpc_id      = "vpc-029ced3d52306d8d9"  # Replace with your VPC ID
+  vpc_id      = "vpc-029ced3d52306d8d9"
 
   ingress {
+    description = "HTTP from VPC"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -20,10 +29,22 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "allow_web_traffic"
+  }
 }
 
-resource "random_string" "suffix" {
-  length  = 5
-  special = false
-  upper   = false
+resource "aws_instance" "web_server" {
+  ami           = "ami-0d266d33ca564bca7"
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.allow_http.name]
+  associate_public_ip_address = true
+
+  key_name = "Key_Pair"
+  
+  tags = {
+    Name = "CD/CI Pipeline"
+  }
 }
