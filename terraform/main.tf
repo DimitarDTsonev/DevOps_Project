3a821d20779c0d37916fd0ec137e76c14fcd1f2d
Dimitar Tsonev <56803540+DimitarDTsonev@users.noreply.github.com>
2025-02-06 16:24:37 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,12 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_instance" "web" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   associate_public_ip_address = true
   key_name                    = var.key_name
 
-  subnet_id                   = "subnet-01bebe580dc2fba32"
+  subnet_id                   = var.subnet_id
 
-  vpc_security_group_ids       = ["sg-076a8ba2eaa7d1401"]
+  vpc_security_group_ids       = [var.security_group_id]
 
   tags = {
     Name = "Flask-Docker-Host"
