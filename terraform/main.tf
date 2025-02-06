e24dd814821cdb5acc973574e9c192481bcbd812
Dimitar Tsonev <56803540+DimitarDTsonev@users.noreply.github.com>
2025-02-06 16:53:56 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  region = var.aws_region
+
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
 resource "aws_instance" "web" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
