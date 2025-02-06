fe45b5e8020ec665c228de848652f76b0f6712be
Dimitar Tsonev <56803540+DimitarDTsonev@users.noreply.github.com>
2025-02-06 15:54:50 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,27 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "web-security-group"
-  description = "Allow HTTP inbound"
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# REMOVE this block since the security group already exists
+# resource "aws_security_group" "web_sg" {
+#   name        = "web-security-group"
+#   description = "Allow HTTP inbound"
+#
+#   ingress {
+#     description = "Allow HTTP"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   egress {
+#     description = "Allow all outbound"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 resource "aws_instance" "web" {
   ami                         = var.ami_id
@@ -29,7 +30,11 @@ resource "aws_instance" "web" {
   associate_public_ip_address = true
   key_name                    = var.key_name
 
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  # Use the existing subnet
+  subnet_id                   = "subnet-01bebe580dc2fba32"
+
+  # Use the existing security group
+  vpc_security_group_ids       = ["sg-076a8ba2eaa7d1401"]
 
   tags = {
     Name = "Flask-Docker-Host"
